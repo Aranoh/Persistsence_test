2fc79a40f86a534e525d8f055732936ef596bc09
Tom Hendriks <tom.hendriks@ibeo-as.com>
2021-05-20 11:55:57 +0200

    upload

---
+++ NewInterfaces/IBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using xmlSpace;
+
+public interface IBase
+{
+    public XmlFieldsBase xmlFieldBase { get; set; }
+}
---
+++ NewInterfaces/ITop.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using xmlSpace;
+
+public interface ITop 
+{
+    public XmlFieldsTop xmlFieldTop { get; set; }
+}
